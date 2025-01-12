96991dd9512ffcc67f26f55658e81260c05e2cf1
sudeep8088 <kumar8088250@gmail.com>
2025-01-12 14:39:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ DB_NAME = environ.get("DB_NAME", "techvjbotz")
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "0")) # Time in Minutes
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "False)")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information

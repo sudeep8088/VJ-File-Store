fdc62ebb83257c85b4a20a3b7c0cc4c0eb9434a4
sudeep8088 <kumar8088250@gmail.com>
2025-01-12 14:24:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -196,7 +196,7 @@ async def start(client, message):
             filesarr.append(msg)
             await asyncio.sleep(1) 
         await sts.delete()
-        if AUTO_DELETE_MODE == True:
+        if AUTO_DELETE_MODE == False:
             k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
             await asyncio.sleep(AUTO_DELETE_TIME)
             for x in filesarr:
@@ -254,7 +254,7 @@ async def start(client, message):
             del_msg = await msg.copy(chat_id=message.from_user.id, caption=f_caption, reply_markup=reply_markup, protect_content=False)
         else:
             del_msg = await msg.copy(chat_id=message.from_user.id, protect_content=False)
-        if AUTO_DELETE_MODE == True:
+        if AUTO_DELETE_MODE == False:
             k = await client.send_message(chat_id = message.from_user.id, text=f"<b><u>❗️❗️❗️IMPORTANT❗️️❗️❗️</u></b>\n\nThis Movie File/Video will be deleted in <b><u>{AUTO_DELETE} minutes</u> 🫥 <i></b>(Due to Copyright Issues)</i>.\n\n<b><i>Please forward this File/Video to your Saved Messages and Start Download there</b>")
             await asyncio.sleep(AUTO_DELETE_TIME)
             try:
